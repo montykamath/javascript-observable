3de0086c7e994ecdada2ff113ef172b16ccebd09
Monty Kamath <monty@goodstart.com>
2015-08-17 15:40:27 -0400

    Switched to QUnit for testing

--- Observable.js
+++ Observable.js
@@ -1,47 +1,51 @@
+"use strict";
 
-// Observable - adds behavior to be observed and notified of events
-function Observable() {
-    this.dependents = [];
-    this._logObservableEvent("Initialized observer: ", this);
-};
+(function(scope) {
+    // Observable - adds behavior to be observed and notified of events
+    function Observable() {
+        this.dependents = [];
+        this._logObservableEvent("Initialized observer: ", this);
+    };
 
-Observable.prototype._logObservableEvent = function() {
-    // logging disabled by default
-    //console.log(arguments);
-};
+    Observable.prototype._logObservableEvent = function() {
+        // logging disabled by default
+        //console.log(arguments);
+    };
 
-Observable.prototype.addObserver = function(obj) {
-    // Make sure this list doesn't have dups
-    for(var i=0; i<this.dependents.length; i++) {
-        if(this.dependents[i] === obj) {
-            this._logObservableEvent("Skipped adding observer because already in list: ", [this, obj]);
-            return;
+    Observable.prototype.addObserver = function(obj) {
+        // Make sure this list doesn't have dups
+        for(var i=0; i<this.dependents.length; i++) {
+            if(this.dependents[i] === obj) {
+                this._logObservableEvent("Skipped adding observer because already in list: ", [this, obj]);
+                return;
+            }
         }
-    }
-    this.dependents.push(obj);
-    this._logObservableEvent("Added observer: ", [this, obj]);
-};
+        this.dependents.push(obj);
+        this._logObservableEvent("Added observer: ", [this, obj]);
+    };
 
-Observable.prototype.removeObserver = function(obj) {
-    for(var i = 0; i < this.dependents.length; i++) {
-      if( this.dependents[i] === obj ) {
-        this.dependents.splice(i, 1);
-        this._logObservableEvent("Removed observer: ", [this, obj]);
-        return true;
-      }
-    }
-    return false;
-};
+    Observable.prototype.removeObserver = function(obj) {
+        for(var i = 0; i < this.dependents.length; i++) {
+          if( this.dependents[i] === obj ) {
+            this.dependents.splice(i, 1);
+            this._logObservableEvent("Removed observer: ", [this, obj]);
+            return true;
+          }
+        }
+        return false;
+    };
 
-Observable.prototype.notify = function() {
-    var args = Array.prototype.slice.call(arguments, 0); //copy args
-    for(var i = 0; i < this.dependents.length; i++) {
-        this._logObservableEvent("Notifying: ", [this, this.dependents[i], args]);
-        this.dependents[i].update.apply(this.dependents[i], args);
-    }
-};
+    Observable.prototype.notify = function() {
+        var args = Array.prototype.slice.call(arguments, 0); //copy args
+        for(var i = 0; i < this.dependents.length; i++) {
+            this._logObservableEvent("Notifying: ", [this, this.dependents[i], args]);
+            this.dependents[i].update.apply(this.dependents[i], args);
+        }
+    };
 
-Observable.prototype.update = function() {
-    this._logObservableEvent("Observed", [this, arguments]);
-};
-
+    Observable.prototype.update = function() {
+        this._logObservableEvent("Observed", [this, arguments]);
+    };
+    
+    scope.Observable = Observable;
+})(window);
--- ObservableTest.js
+++ ObservableTest.js
@@ -1,45 +1,33 @@
+"use strict";
 
-// ObservableTest - test case that tests the bahavior of Observable objects
-function ObservableTest() {
-    this.eventsReceived = [];
-};
-
-ObservableTest.prototype.test1 = function() {
-    // Run this test method by adding this to your html:
-    //     <script type-"text/javascript">
-    //          (new ObservableTest()).test1();
-    //     </script>
-   
-    // Instantiate an observable
-    var observable = new Observable();
+(function(){
     
-    // Make me an observer
-    observable.addObserver(this);
-    // Confirm that observer was added
-    this.assert(observable.dependents.length == 1, "Observer not added");
+    QUnit.test("Observable test", function( assert ) {
+        var eventsReceived = [];
+        var observable = new window.Observable();
+        
+        var observer = {};
+        observer.update = function() {
+            // When an this observer receives an event, add it to the list of events
+            eventsReceived.push(arguments);
+        };
+        
+        // Make me an observer
+        observable.addObserver(observer);
+        // Confirm that observer was added
+        assert.ok(observable.dependents.length == 1, "Check observer added");
+        
+        // Trigger an event
+        observable.notify("EventName", "some other arg", 1);
+        // Confirm that we recieve the event including all of the arguments
+        assert.ok(eventsReceived[0][0] == "EventName", "Check 1st arg");
+        assert.ok(eventsReceived[0][1] == "some other arg", "Check 2nd arg");
+        assert.ok(eventsReceived[0][2] == 1, "Check 3rd arg");
+        
+        // Remove me as an observer
+        observable.removeObserver(observer);
+        // Confirm that observer was removed
+        assert.ok(observable.dependents.length == 0, "Check observer removed");
+    });
     
-    // Trigger an event
-    observable.notify("EventName", "some other arg", 1);
-    // Confirm that we recieve the event including all of the arguments
-    this.assert(this.eventsReceived[0][0] == "EventName", "1st argument doesn't match");
-    this.assert(this.eventsReceived[0][1] == "some other arg", "2nd argument doesn't match");
-    this.assert(this.eventsReceived[0][2] == 1, "3rd argument doesn't match");
-    
-    // Remove me as an observer
-    observable.removeObserver(this);
-    // Confirm that observer was removed
-    this.assert(observable.dependents.length == 0, "Observer not removed");
-};
-
-ObservableTest.prototype.assert = function(condition, message) {
-    // Assert that the condition is true.  If it is false, throw an exception with the message
-    if (!condition) {
-        throw message || "Assertion failed";
-    }
-};
-
-ObservableTest.prototype.update = function() {
-    // When an observable notifies us about an event, 
-    // just add it to our list of events so we can verify it later
-    this.eventsReceived.push(arguments);
-};
+})();
